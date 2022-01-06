0592d864c254c30689134bd780b7fb56ebf2ca45
Evan Almeida <evan.almeidaf@gmail.com>
2022-01-06 17:10:32 -0500

    🚧 feat (socket): apply socket context changes to room page, add ability to see currently connected users

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,13 @@
-const { createServer } = require("http");
-const { Server } = require("socket.io");
+import { createServer } from "http";
+import { Server } from "socket.io";
+const express = require("express");
+const cors = require("cors");
 
-const httpServer = createServer();
+const app = express();
+const httpServer = createServer(app);
+
+app.use(cors());
+
 const io = new Server(httpServer, {
   cors: {
     origin: "*",
@@ -10,19 +16,27 @@ const io = new Server(httpServer, {
 });
 
 io.on("connection", (socket: any) => {
-  socket.join("testRoom");
-
-  socket.to("testRoom").emit(`joined`, socket.id);
-
   socket.on("message", (data: any) => {
     io.in("testRoom").emit("message", data);
   });
 
-  // socket.on("set-username", (name: any) => {
-  //   socket.username = name;
-  // });
+  socket.on("set_name", (name: string) => {
+    socket.username = name;
+  });
 
-  // console.log(io.of("/").adapter.rooms.get("testRoom").size);
+  socket.on("join_room", (roomId: string) => {
+    io.in(roomId).emit("joined_room");
+    socket.join(roomId);
+  });
+});
+
+app.get(`/users/:roomId`, (req: any, res: any) => {
+  let params = req.params;
+  let roomId = params.roomId;
+  let users = io.of("/").adapter.rooms.get(roomId);
+  // console.log(io.of("/").adapter.rooms);
+  // console.log(users.values());
+  res.send(Array.from(users.values()));
 });
 
 httpServer.listen(5000);
--- frontend/pages/room/[roomId]/index.tsx
+++ frontend/pages/room/[roomId]/index.tsx
@@ -1,77 +1,108 @@
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { io } from "socket.io-client";
+import { SocketContext } from "../../../contexts/SocketContext";
+import axios from "axios";
 
 const Room = () => {
   const router = useRouter();
-  const roomCode = router.query.roomId;
+  const roomId = router.query.roomId as string;
   const [isConnected, setIsConnected] = useState<boolean>(false);
   const [message, setMessage] = useState("");
   const [messages, setMessages] = useState<string[]>([]);
   const [socket, setSocket] = useState<any>(null);
+  const [usersConnected, setUsersConnected] = useState<object[]>([]);
+  const socketContext = useContext(SocketContext);
+
+  const getConnectedUsers = async () => {
+    await axios
+      .get(`http://localhost:5000/users/${roomId}`)
+      .then((res: any) => {
+        setUsersConnected(res.data);
+      });
+  };
 
   useEffect(() => {
-    let newSocket = io("ws://localhost:5000?room=test");
+    getConnectedUsers();
+  }, [roomId]);
 
-    setSocket(newSocket);
+  useEffect(() => {
+    if (socketContext.socket != null) {
+      console.log("joining room", roomId);
+      socketContext.joinRoom(roomId);
+    }
+  }, [socketContext]);
 
-    newSocket.on("connect", () => {
-      setIsConnected(true);
-      console.log("connected");
-    });
-
-    newSocket.on("joined", (arg) => {
-      console.log("someone joined", arg);
-    });
-  }, []);
-
-  if (socket != null) {
-    socket.on("message", (data: any) => {
-      // console.log(data, messages, newSocket.id);
+  if (socketContext.socket != null) {
+    socketContext.socket.on("message", (data: any) => {
       let newMessages = [...messages];
       newMessages.push(data);
       setMessages(newMessages);
     });
+
+    socketContext.socket.on("connect", () => {
+      setIsConnected(true);
+      console.log("connected");
+    });
+
+    socketContext.socket.on("joined_room", (arg: any) => {
+      console.log("new-join-room");
+      getConnectedUsers();
+    });
   }
 
   return (
-    <div className="min-h-screen flex flex-col justify-center items-center">
-      <div className="mb-4">
-        Your socket id is: {isConnected ? socket.id : "Loading..."}
-      </div>
-      <div className=" bg-gray-400 rounded-md p-12 flex flex-col">
-        {/* <div></div> */}
-        {/* <div className="text-black">
-          {isConnected ? "Connected!" : "Connecting..."}
-        </div> */}
-        <div>
-          <div className="text-black-400">
-            {messages.map((message) => {
-              return (
-                <div
-                  className="mb-4 bg-button-bg text-white pl-2 py-1 rounded-full"
-                  key={message}
-                >
-                  {message}
-                </div>
-              );
-            })}
+    <div className="flex items-center justify-center gap-4">
+      <div className="min-h-screen flex flex-col justify-center items-center">
+        <div className="mb-4">
+          Socket context id is{" "}
+          {socketContext ? socketContext?.socket?.id : "Loading..."}
+        </div>
+        <div className=" bg-gray-400 rounded-md p-12 flex flex-col">
+          <div>
+            <div className="text-black-400">
+              {messages.map((message) => {
+                return (
+                  <div>
+                    <div className="flex justify-between">
+                      <div className="text-gray-500">Test Name</div>
+                      <div className="text-gray-500">Thurs 23 2021</div>
+                    </div>
+
+                    <div
+                      className="mb-4 bg-button-bg text-white pl-2 py-1 rounded-full"
+                      key={message}
+                    >
+                      {message}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div className="flex gap-2">
+            <input
+              className="w-full h-8 rounded-full pl-2"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+            <button
+              onClick={() => {
+                socketContext.socket.send(message);
+                setMessage("");
+              }}
+            >
+              Enter
+            </button>
           </div>
         </div>
-        <div className="flex gap-2">
-          <input
-            className="w-full h-8 rounded-full pl-2"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-          />
-          <button
-            onClick={() => {
-              socket.send(message);
-              setMessage("");
-            }}
-          >
-            Enter
-          </button>
+      </div>
+      <div className="flex flex-col items-center justify-center">
+        <div className="underline">Users Connected</div>
+        <div>
+          {Object.values(usersConnected).map((user) => {
+            return <div>{user}</div>;
+          })}
         </div>
       </div>
     </div>
